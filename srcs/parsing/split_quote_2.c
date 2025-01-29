5738e983e4b20215d5f4e97e79960b1c60714fda
Pol <polepie@gmail.com>
2025-01-29 02:07:57 +0100

    Split quote

--- srcs/parsing/split_quote_2.c
+++ srcs/parsing/split_quote_2.c
@@ -169,8 +169,9 @@ int	sq_replace_and_free(t_list *elements, t_list **ret)
 	}
 	if (tmp_exec->argv)
 		tmp_exec->argv[i] = NULL;
-	if (!can_error)
-		ft_lstadd_back(ret, ft_lstnew(tmp_exec));
+		ft_printf("add back %d\n", can_error);////
+	//if (!can_error)
+	ft_lstadd_back(ret, ft_lstnew(tmp_exec));
 	return (0);
 }
 
