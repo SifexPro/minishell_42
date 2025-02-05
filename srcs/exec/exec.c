6c173ea4a467e267a533451a80f6c3a21198e79d
Pol <polepie@gmail.com>
2025-02-05 23:22:35 +0100

    Auto stash before merge of "norme-parsing" and "origin/norme-parsing"

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -145,7 +145,7 @@ int	parse_cmd(char *input, t_ht *env, char **envp, int last_status)
 		printf("((t_exec *)splitted->content)->token_next: %d\n", ((t_exec *)splitted->content)->token_next);////
 		splitted = splitted->next;
 	}
-	splitted = temp_list;*/
+	splitted = temp_list; */
 	////
 
 	flags = set_flags(splitted);
